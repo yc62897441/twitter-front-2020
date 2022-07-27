e3cb393a571b1b5abf24f373e094b37202992105
yc62897441 <yc62897441@gmail.com>
2022-07-27 11:07:44 +0800

    vue專案初始化、安裝vue router、載入bootstrap

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import NotFound from '../views/NotFound.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // 動態載入 => 這樣的寫法可以減少不必要的流量：如果使用者沒有到達這裡、那就不用預先載入這頁的組件內容。
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '*',
+    name: 'not-found',
+    component: NotFound,
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
