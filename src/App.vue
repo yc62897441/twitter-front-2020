a86b4e0c48599074d63a492b392caf2af1366a8a
yc62897441 <yc62897441@gmail.com>
2022-07-28 09:42:32 +0800

    整理 signin、signup 的 style 到同一個 css 檔

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 // 全域樣式可以外部引入
 import '../src/style.css'
+import '../src/style_signin_signup.css'
 export default {
   name: 'App'
 }
